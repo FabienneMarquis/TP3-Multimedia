6601b8ee6e5f3c5c0109f31df5ae6b68f1616454
FabienneMarquis <fabienne.marquis@gmail.com>
2016-03-03 13:01:25 -0500

    début étape 2 en cours d'ajout d'animation

--- src/views/MusiquePaneCtrl.java
+++ src/views/MusiquePaneCtrl.java
@@ -21,7 +21,9 @@ import utils.WindowApp;
 
 public class MusiquePaneCtrl extends WindowApp {
 
-	 public static void main(String[] args) { launch(args); }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @FXML
     private HBox root;
@@ -33,128 +35,143 @@ public class MusiquePaneCtrl extends WindowApp {
     private Label lblMusique;
 
     @FXML
-    private VBox playPaneSlot;    
-    
-	@FXML
+    private VBox playPaneSlot;
+
+    @FXML
     private CheckBox checkRejouer;
-	
-	private AnimationPaneCtrl animationPaneCtrl1 = null;
-	private AnimationPaneCtrl animationPaneCtrl2 = null;
-	private AnimationPaneCtrl animationPaneCtrl3 = null;
-	private AnimationPaneCtrl animationPaneCtrl4 = null;
 
-	private PlayPaneCtrl playPaneCtrl = null;
-	
+    @FXML
+    private VBox vBoxRigth;
+
+    @FXML
+    private VBox vboxLeft;
+
+    private AnimationPaneCtrl animationPaneCtrl1 = null;
+    private AnimationPaneCtrl animationPaneCtrl2 = null;
+    private AnimationPaneCtrl animationPaneCtrl3 = null;
+    private AnimationPaneCtrl animationPaneCtrl4 = null;
+
+    private PlayPaneCtrl playPaneCtrl = null;
+
     private Media media = null;
-	private String acdcBiB= "ACDC - Back In Black.mp3";
+    private String acdcBiB = "ACDC - Back In Black.mp3";
 
-    private void initAnimation()
-    {
-    	animationPaneCtrl1 = (new AnimationPaneCtrl()).loadView();
-		animationPaneCtrl2 = (new AnimationPaneCtrl()).loadView();
-		root.getChildren().addAll(animationPaneCtrl1.getRoot(),animationPaneCtrl2.getRoot());
+    private void initAnimation() {
+        animationPaneCtrl1 = (new AnimationPaneCtrl()).loadView();
+        animationPaneCtrl2 = (new AnimationPaneCtrl()).loadView();
+        animationPaneCtrl3 = (new AnimationPaneCtrl()).loadView();
+        animationPaneCtrl4 = (new AnimationPaneCtrl()).loadView();
+
+        vboxLeft.getChildren().add( animationPaneCtrl3.getRoot());
+        vboxLeft.getChildren().add(animationPaneCtrl4.getRoot());
+        vBoxRigth.getChildren().add(animationPaneCtrl1.getRoot());
+        vBoxRigth.getChildren().add(animationPaneCtrl2.getRoot());
     }
-    
-    private void initDragAndDrop()
-    {
-    	controlPane.setOnDragDropped( event -> {
-	    	Dragboard db = event.getDragboard();
-	        String erreur = null;
-	        if (db.hasFiles())
-	        {
-	        	File f = db.getFiles().get(0);
-	        	try {
-		        	Media newMedia = new Media(f.toURI().toString());
-		            
-		        	if( newMedia.getError()!= null )
-		        	{
-		        		erreur = "Impossible de charger le fichier: " + f.getName();
-		        	}
-		        	else 
-		        	{
-		        		playPaneCtrl.bindSong(newMedia, f.getName());
-		        	}
-	        	}
-	        	catch(MediaException e)
-	        	{
-	        		erreur = "Impossible de charger le fichier: " + f.getName() + " ( " + e.getMessage() + " )";
-	        	}
-	        }
-	        if(erreur != null){
-	        	Alert alert = new Alert(AlertType.ERROR, erreur);
-	        	alert.showAndWait();        		        	
-	        }
-	        event.setDropCompleted(erreur != null);
-			controlPane.getStyleClass().remove("DragOver");
-	        event.consume();    	
-	    });
-    	
-    	controlPane.setOnDragEntered( event -> {
-	    	if (event.getDragboard().hasFiles()) 
-	    	{
-	    		controlPane.getStyleClass().add("DragOver");
-	            event.consume();    	
-	        }
-	    });
-    	
-    	controlPane.setOnDragExited( event -> {
-			controlPane.getStyleClass().remove("DragOver");
-			event.consume();    	
-	    });
-    	
-    	controlPane.setOnDragOver( event -> {
-			if ( event.getDragboard().hasFiles() ) 
-			{
-	            event.acceptTransferModes(TransferMode.COPY);
-	            event.consume();
-	        }
-	    });
-   	
+
+    private void initDragAndDrop() {
+        controlPane.setOnDragDropped(event -> {
+            Dragboard db = event.getDragboard();
+            String erreur = null;
+            if (db.hasFiles()) {
+                File f = db.getFiles().get(0);
+                try {
+                    Media newMedia = new Media(f.toURI().toString());
+
+                    if (newMedia.getError() != null) {
+                        erreur = "Impossible de charger le fichier: " + f.getName();
+                    } else {
+                        playPaneCtrl.bindSong(newMedia, f.getName());
+                    }
+                } catch (MediaException e) {
+                    erreur = "Impossible de charger le fichier: " + f.getName() + " ( " + e.getMessage() + " )";
+                }
+            }
+            if (erreur != null) {
+                Alert alert = new Alert(AlertType.ERROR, erreur);
+                alert.showAndWait();
+            }
+            event.setDropCompleted(erreur != null);
+            controlPane.getStyleClass().remove("DragOver");
+            event.consume();
+        });
+
+        controlPane.setOnDragEntered(event -> {
+            if (event.getDragboard().hasFiles()) {
+                controlPane.getStyleClass().add("DragOver");
+                event.consume();
+            }
+        });
+
+        controlPane.setOnDragExited(event -> {
+            controlPane.getStyleClass().remove("DragOver");
+            event.consume();
+        });
+
+        controlPane.setOnDragOver(event -> {
+            if (event.getDragboard().hasFiles()) {
+                event.acceptTransferModes(TransferMode.COPY);
+                event.consume();
+            }
+        });
+
     }
 
     @FXML
-    private void initialize()
-    {
-    	initAnimation();
-    	initPlayPane();
-    	initDragAndDrop();
+    private void initialize() {
+        initAnimation();
+        initPlayPane();
+        initDragAndDrop();
     }
-    
-    private void initPlayPane()
-    {
-    	URL musique = getClass().getResource("/ressources/"+acdcBiB);
-    	media = new Media(musique.toExternalForm());
-    	
-    	playPaneCtrl = (new PlayPaneCtrl()).loadView();
-    	playPaneSlot.getChildren().add(playPaneCtrl.getRoot());
-    	VBox.setVgrow(playPaneCtrl.getRoot(), Priority.SOMETIMES);
-    	playPaneCtrl.config( 
-    			newStatus->onPlayingStatusChange(newStatus), 
-    			()->onEndOfMedia());
-    	playPaneCtrl.bindSong(media, acdcBiB);
-    	animationPaneCtrl1.bindRate(playPaneCtrl.volumeProperty());
-		animationPaneCtrl2.bindRate(playPaneCtrl.volumeProperty());
+
+    private void initPlayPane() {
+        URL musique = getClass().getResource("/ressources/" + acdcBiB);
+        media = new Media(musique.toExternalForm());
+        initAnimation();
+        playPaneCtrl = (new PlayPaneCtrl()).loadView();
+        playPaneSlot.getChildren().add(playPaneCtrl.getRoot());
+        VBox.setVgrow(playPaneCtrl.getRoot(), Priority.SOMETIMES);
+        playPaneCtrl.config(
+                newStatus -> onPlayingStatusChange(newStatus),
+                () -> onEndOfMedia());
+        playPaneCtrl.bindSong(media, acdcBiB);
+        animationPaneCtrl1.bindRate(playPaneCtrl.volumeProperty());
+        animationPaneCtrl2.bindRate(playPaneCtrl.volumeProperty());
+        animationPaneCtrl3.bindRate(playPaneCtrl.volumeProperty());
+        animationPaneCtrl4.bindRate(playPaneCtrl.volumeProperty());
     }
-    
-    private void onEndOfMedia()
-    {
-		if(checkRejouer.isSelected()) playPaneCtrl.bindAndPlay(media, "Popcorn");
+
+    private void onEndOfMedia() {
+        if (checkRejouer.isSelected()) playPaneCtrl.bindAndPlay(media, "Popcorn");
     }
-    
-    private void onPlayingStatusChange(Status newStatus)
-    {
-		if(newStatus == Status.PLAYING){ animationPaneCtrl1.play(); animationPaneCtrl2.play();}
-		else {animationPaneCtrl1.pause(); animationPaneCtrl2.pause();}
+
+    private void onPlayingStatusChange(Status newStatus) {
+        if (newStatus == Status.PLAYING) {
+            animationPaneCtrl1.play();
+            animationPaneCtrl2.play();
+            animationPaneCtrl3.play();
+            animationPaneCtrl4.play();
+        } else {
+            animationPaneCtrl1.pause();
+            animationPaneCtrl2.pause();
+            animationPaneCtrl3.pause();
+            animationPaneCtrl4.pause();
+        }
     }
 
     @Override
-	protected String getFxml() { return "MusiquePane.fxml"; }
+    protected String getFxml() {
+        return "MusiquePane.fxml";
+    }
 
     @Override
-	protected Parent getRoot() { return root; }
-    
-	@Override
-	protected String getTitle() { return "TP3 - Musique - Fabienne Marquis"; }
-    
+    protected Parent getRoot() {
+        return root;
+    }
+
+    @Override
+    protected String getTitle() {
+        return "TP3 - Musique - Fabienne Marquis";
+    }
+
 }
 
